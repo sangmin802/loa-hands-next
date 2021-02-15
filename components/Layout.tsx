5e3c405b8262f2fe864c71e2bd36bb346c7f3f7c
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-02-15 21:55:14 +0900

    21-02-15/21:55

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,6 +24,8 @@ const Layout : React.FC<Props> = ({
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="google-site-verification" content="SWvP4IgjDhYd943otN_bOiXQ3UjJsOAh5FE6cpmrkXE" />
+        <meta name="description" content="Lostark 유저 검색 앱" />
       </Head>
       <div className={`App ${isLoadingStyle1}`}>
         <LoadingSpinner isLoadingStyle={isLoadingStyle2}/>
