c2ef737ea08ed86a330ea06a1c5fc27060361447
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-15 16:48:22 +0900

    필요 Provider 추가 1. ThemeProvider 2. QueryClient
    QueryClient 속성에서 suspense 속성은 false로 변경
    1 아직 Suspense를 SSR에서는 지원하지 못하는 상태
    2. useQuery의 Status 상태를 통해 Suspense 컴포넌트를 직접 구성해야 할 듯

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
-import "style/App.css";
-import "style/index.css";
-import "style/UserInfo.css";
 import React from "react";
 import { AppProps } from "next/app";
 import { wrapper } from "store/index";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle, { THEME } from "global-style";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      // suspense: true,
+      useErrorBoundary: true,
+      cacheTime: 1000 * 60 * 5,
+      staleTime: 1000 * 60 * 3,
+    },
+  },
+});
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={THEME}>
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 };
 
 export default wrapper.withRedux(App);
