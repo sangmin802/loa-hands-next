27bb85e8c8ff930658227a5b80b691ea0c0ae0e7
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-16 16:24:21 +0900

    next.config 업데이트 1. jsdom 클라이언트사이드에서는 작동 x

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        dgram: "empty",
-        fs: "empty",
-        net: "empty",
-        tls: "empty",
-        child_process: "empty",
-      };
-    } else {
-    }
-    return config;
-  },
-};
-
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.node = {
+//         dgram: "empty",
+//         fs: "empty",
+//         net: "empty",
+//         tls: "empty",
+//         child_process: "empty",
+//       };
+//     } else {
+//     }
+//     return config;
+//   },
+// };
 const withCSS = require("@zeit/next-css");
+
 module.exports = withCSS(() => {
   return {
     cssModules: true,
@@ -23,4 +23,31 @@ module.exports = withCSS(() => {
       localIdentName: "[local]___[hash:base64:5]",
     },
   };
+});
+
+module.exports = withCSS({
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].ext",
+        },
+      },
+    });
+
+    if (!isServer) {
+      config.node = {
+        dgram: "empty",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty",
+      };
+    }
+
+    return config;
+  },
 });
