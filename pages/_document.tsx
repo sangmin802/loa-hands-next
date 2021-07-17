ad6add2e914824b972e745b2f17dc45de8556d0c
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-17 10:27:51 +0900

    새로고침으로 잘못된 route로 이동 시, 처음부터 다시 경로를 찾도록 함

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,25 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
+    const reload = `
+    (function (l) {
+      if (l.search[1] === "/") {
+        var decoded = l.search
+          .slice(1)
+          .split("&")
+          .map(function (s) {
+            return s.replace(/~and~/g, "&");
+          })
+          .join("?");
+
+        window.history.replaceState(null, null, decoded + l.hash);
+      }
+    })(window.location);
+    `;
     return (
       <Html>
         <Head>
+          <script type="text/javascript">{reload}</script>
           <link rel="stylesheet" href="/fonts/fonts.css" />
         </Head>
         <body>
